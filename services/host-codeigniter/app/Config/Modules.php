d0bd5c2f67273d736da7213d29952893cc81da97
giomatamm20 <giomatamm20@users.noreply.github.com>
2026-06-07 15:30:10 -0600

    Align CodeIgniter modules property signatures

--- services/host-codeigniter/app/Config/Modules.php
+++ services/host-codeigniter/app/Config/Modules.php
@@ -6,7 +6,7 @@ use CodeIgniter\Modules\Modules as BaseModules;
 
 class Modules extends BaseModules
 {
-    public bool $enabled = true;
-    public bool $discoverInComposer = true;
-    public array $aliases = [];
+    public $enabled = true;
+    public $discoverInComposer = true;
+    public $aliases = [];
 }
